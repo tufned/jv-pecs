3042ffd3519303afe14d95b58e5966292a3c9dca
Denys Funderat <funderatdenis@gmail.com>
2025-10-15 14:18:30 +0300

    fixes

--- src/main/java/core/mate/academy/service/MachineService.java
+++ src/main/java/core/mate/academy/service/MachineService.java
@@ -7,7 +7,6 @@ import java.util.List;
  * Parameterize this service and add its implementation in a separate class.
  */
 public interface MachineService<T extends Machine> {
-
     /**
      * Return the list of machines.
      * In the implementation of this method please use your MachineProducer implementations
--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -4,6 +4,7 @@ import core.mate.academy.model.Bulldozer;
 import core.mate.academy.model.Excavator;
 import core.mate.academy.model.Machine;
 import core.mate.academy.model.Truck;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -12,23 +13,19 @@ import java.util.List;
 public class MachineServiceImpl<T extends Machine> implements MachineService<T> {
     @Override
     public List<T> getAll(Class<? extends Machine> type) {
-        MachineProducer<? extends Machine> p;
-        List<? extends Machine> list;
+        List<? extends Machine> machines;
 
-        if (type == Bulldozer.class) {
-            p = new BulldozerProducerImpl();
-            list = p.get();
-            return (List<T>) list;
-        } else if (type == Truck.class) {
-            p = new TruckProducerImpl();
-            list = p.get();
-            return (List<T>) list;
-        } else if (type == Excavator.class) {
-            p = new ExcavatorProducerImpl();
-            list = p.get();
-            return (List<T>) list;
+        if (type.equals(Bulldozer.class)) {
+            machines = new BulldozerProducerImpl().get();
+        } else if (type.equals(Truck.class)) {
+            machines = new TruckProducerImpl().get();
+        } else if (type.equals(Excavator.class)) {
+            machines = new ExcavatorProducerImpl().get();
+        } else {
+            return Collections.emptyList();
         }
-        return List.of();
+
+        return (List<T>) machines;
     }
 
     @Override
